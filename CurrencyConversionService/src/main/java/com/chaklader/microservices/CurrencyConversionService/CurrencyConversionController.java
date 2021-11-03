b1584a07456f20c8abc96f947e72434935bb6363
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2021-11-03 16:03:16 +0600

    1. added shell script to create docker images 2. docker composer file is provided to create the micro-service architecture

--- CurrencyConversionService/src/main/java/com/chaklader/microservices/CurrencyConversionService/CurrencyConversionController.java
+++ CurrencyConversionService/src/main/java/com/chaklader/microservices/CurrencyConversionService/CurrencyConversionController.java
@@ -21,29 +21,29 @@ public class CurrencyConversionController {
     @Autowired
     private CurrencyExchangeProxy exchangeProxy;
 
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversion(
-        @PathVariable String from,
-        @PathVariable String to,
-        @PathVariable BigDecimal quantity
-    ) {
-
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-
-
-        final ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-            CurrencyConversion.class, uriVariables);
-
-        final CurrencyConversion currencyConversion = responseEntity.getBody();
-
-        return new CurrencyConversion(currencyConversion.getId(),
-            from, to, quantity,
-            currencyConversion.getConversionMultiple(),
-            quantity.multiply(currencyConversion.getConversionMultiple()),
-            currencyConversion.getEnvironment() + " " + "rest template");
-    }
+//    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+//    public CurrencyConversion calculateCurrencyConversion(
+//        @PathVariable String from,
+//        @PathVariable String to,
+//        @PathVariable BigDecimal quantity
+//    ) {
+//
+//        Map<String, String> uriVariables = new HashMap<>();
+//        uriVariables.put("from", from);
+//        uriVariables.put("to", to);
+//
+//
+//        final ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+//            CurrencyConversion.class, uriVariables);
+//
+//        final CurrencyConversion currencyConversion = responseEntity.getBody();
+//
+//        return new CurrencyConversion(currencyConversion.getId(),
+//            from, to, quantity,
+//            currencyConversion.getConversionMultiple(),
+//            quantity.multiply(currencyConversion.getConversionMultiple()),
+//            currencyConversion.getEnvironment() + " " + "rest template");
+//    }
 
     @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversionFeign(
